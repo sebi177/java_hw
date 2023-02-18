43138fe71f2219fc468c66e4cd07bcf658a692a8
Eusebiu Jacot <124197015+sebi177@users.noreply.github.com>
2023-02-18 10:47:53 +0100

    HomewokTwo

--- HomeworkTwoPartOne.java
+++ HomeworkTwoPartOne.java
@@ -18,5 +18,13 @@ public class HomeworkTwoPartOne {
         System.out.println(d);
         System.out.println(l);
 
+        int n = 365;
+        int n1 = n / 10;
+
+        System.out.println( n + "  % " + " 10 " + " =  " + ( n % 10 ));
+        System.out.println( n + "  / " + " 10 " + " =  " + ( n / 10 ));
+        System.out.println( n1 + "  % " + " 10 " + " =  " + ( n % 10 ));
+        System.out.println( n + " / " + " 10 " + " / " + " 10 " + " =  " + ( n / 10 / 10 ));
+
     }
 }
