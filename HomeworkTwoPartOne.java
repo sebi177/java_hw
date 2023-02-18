da3f6278f78f388ba5314122711ae24a4afd2678
Eusebiu Jacot <124197015+sebi177@users.noreply.github.com>
2023-02-18 10:45:11 +0100

    HomeworkTwo

---
+++ HomeworkTwoPartOne.java
@@ -0,0 +1,22 @@
+package Homework;
+
+public class HomeworkTwoPartOne {
+    public static void main(String[] args){
+        char c = 'G';
+        int i = 89;
+        byte b = 4;
+        short s = 56;
+        float f = 4.7333436f;
+        double d = 4.355453532;
+        long l = 12121;
+
+        System.out.println(c);
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(l);
+
+    }
+}
---
+++ HomeworkTwoPartTwo.java
@@ -0,0 +1,14 @@
+package Homework;
+
+public class HomeworkTwoPartTwo {
+    public static void main(String[] aargs){
+
+        int n = 365;
+        int n1 = n / 10;
+
+        System.out.println( n + "  % " + " 10 " + " =  " + ( n % 10 ));
+        System.out.println( n + "  / " + " 10 " + " =  " + ( n / 10 ));
+        System.out.println( n1 + "  % " + " 10 " + " =  " + ( n % 10 ));
+        System.out.println( n + " / " + " 10 " + " / " + " 10 " + " =  " + ( n / 10 / 10 ));
+    }
+}
